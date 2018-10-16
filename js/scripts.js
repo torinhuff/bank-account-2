c59c32177dc19fe9605d238d63239b0dcae94f11
Student <example@example.com>
2018-10-15 17:55:39 -0700

    add initial HTML

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+//business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
